8f1b6896af52f739786ca6aa07c9d0bcf3c6e1a4
fabio <fabiowilliam20111@gmail.com>
2022-02-18 22:41:59 -0300

    first commit

--- atividadeonline.js
+++ atividadeonline.js
@@ -1,4 +1,22 @@
-const nascimento = 1995
-var data = 2022
-console.log (data - nascimento)
-console.log (0.7 + 0.1)
+var listaDePecas = ["Amortecedo", "Motor", "Filtro de Ar"]
+let peso = 50
+if (peso < 100) {
+    console.log("A peça tem que pesar no minimo 100g")
+ }
+ else {
+     console.log("Pode cadastrar")
+ }
+ if (listaDePecas.length < 10) {
+     console.log("ainda pode cadastrar mais")
+ }
+ else {
+     console.log("a caixa já está cheia")
+ }
+ let nomePeca = "Disco de Freio"
+ if (nomePeca.length > 3){
+     console.log("O nome da peça está adequado")
+ } else if (nomePeca == 0){
+     console.log("O nome da peça não pode ser vazio")
+ } else {
+     console.log("O nome da peça deve ter mais de 3 caracteres, digite um nome adequado")
+ }
